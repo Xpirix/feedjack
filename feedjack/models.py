59b535e8d6f0dcaf50528a556e2bbff7f12d6a76
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2023-11-21 08:59:44 +0300

    Update models.py for Django 4

--- feedjack/models.py
+++ feedjack/models.py
@@ -8,7 +8,7 @@ models.py
 """
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _ 
+from django.utils.translation import gettext_lazy as _ 
 from django.utils.encoding import smart_text
 
 from feedjack import fjcache
